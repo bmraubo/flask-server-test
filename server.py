873a7e7b56fac72d422bceb1906ff9825a3df783
bmraubo <bmraubo@gmail.com>
2021-10-19 18:09:11 +0100

    defining host and port

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import re
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def process_JSON():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
